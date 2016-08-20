67065f9fda2e429cab3a6cef9e78e83ddd90d92c
Tiago Maluta <tiago.maluta@gmail.com>
2016-08-20 10:57:05 -0300

    +
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
 app.get('/', function(request, response) {
   //response.render('pages/index');
   responde.render("oi")
 });
+
+app.get('/x', function(request, response) {
+  //response.render('pages/index');
+  responde.render("oi")
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
